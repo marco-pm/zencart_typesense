b0c4e75ee23a5c0752a8ed870e23396f8ad2267d
Marco Ponchia <marco.ponchia@marcopm.net>
2023-02-10 19:58:02 +0100

    Update installer

--- zc_plugins/Typesense/v1.0.0/Installer/ScriptedInstaller.php
+++ zc_plugins/Typesense/v1.0.0/Installer/ScriptedInstaller.php
@@ -25,22 +25,47 @@ class ScriptedInstaller extends ScriptedInstallBase
      */
     public function doInstall(): bool
     {
-        // TODO: check that Instant Search is installed and has a version >= 4.0.0
+        global $messageStack;
 
+        // Check that Instant Search plugin is installed and has a version >= 4
         $sql = $this->dbConn->Execute("
-            SELECT configuration_group_id
-            FROM " . TABLE_CONFIGURATION_GROUP . "
-            WHERE configuration_group_title = '" . self::CONFIGURATION_GROUP_TITLE . "'
-            LIMIT 1
+            SELECT
+                version
+            FROM
+                " . TABLE_PLUGIN_CONTROL . "
+            WHERE
+                unique_key = 'InstantSearch'
+                AND version NOT REGEXP '^v[2-3]'
+            LIMIT
+                1
         ");
         if ($sql->RecordCount() === 0) {
-            // todo print error message
+            $errorMsg = 'The Typesense add-on requires Instant Search plugin v4 (or later) installed. Please install Instant Search plugin first.';
+            $this->errorContainer->addError(1, $errorMsg, true, $errorMsg);
+            return false;
+        }
+
+        $sql = $this->dbConn->Execute("
+            SELECT
+                configuration_group_id
+            FROM
+                " . TABLE_CONFIGURATION_GROUP . "
+            WHERE
+                configuration_group_title = '" . self::CONFIGURATION_GROUP_TITLE . "'
+            LIMIT
+                1
+        ");
+        if ($sql->RecordCount() === 0) {
+            $errorMsg = 'Instant Search plugin is not installed properly. Please re-install Instant Search plugin first.';
+            $this->errorContainer->addError(1, $errorMsg, true, $errorMsg);
             return false;
         }
 
         $configurationGroupId = (int)($sql->fields['configuration_group_id']);
 
         $this->createConfigurationSettings($configurationGroupId);
+
+        $this->createSyncTable();
 
         return true;
     }
@@ -52,11 +77,16 @@ class ScriptedInstaller extends ScriptedInstallBase
      */
     public function doUninstall(): bool
     {
-        // TODO
+        $sql = "
+            DELETE FROM
+                " . TABLE_CONFIGURATION . "
+            WHERE
+                configuration_key LIKE 'TYPESENSE_%'
+                OR configuration_key = 'INSTANT_SEARCH_ENGINE'
+        ";
+        $this->executeInstallerSql($sql);
 
-        $sql = "DELETE FROM " . TABLE_CONFIGURATION . "
-                WHERE configuration_key LIKE 'TYPESENSE_%'
-                OR configuration_key = 'INSTANT_SEARCH_ENGINE'";
+        $sql = "DROP TABLE IF EXISTS " . DB_PREFIX . "typesense_sync_status";
         $this->executeInstallerSql($sql);
 
         return true;
@@ -70,6 +100,7 @@ class ScriptedInstaller extends ScriptedInstallBase
      */
     protected function createConfigurationSettings(int $configurationGroupId): void
     {
+        // TODO settings descriptions
         $sql = "
             INSERT INTO " . TABLE_CONFIGURATION . "
                 (configuration_title, configuration_key, configuration_value, configuration_description, configuration_group_id, date_added, sort_order, use_function, set_function, val_function)
@@ -84,6 +115,41 @@ class ScriptedInstaller extends ScriptedInstallBase
                 ('[Typesense] Sync Collections even if the last Sync did not complete successfully', 'TYPESENSE_SYNC_AFTER_FAILED', 'true', 'TODO', $configurationGroupId, now(), 5600, NULL, 'zen_cfg_select_option(array(\'true\', \'false\'),', NULL),
                 ('[Typesense] Sync Timeout (minutes)', 'TYPESENSE_SYNC_TIMEOUT_MINUTES', '30', 'TODO', $configurationGroupId, now(), 5700, NULL, NULL, '{\"error\":\"TEXT_INSTANT_SEARCH_CONFIGURATION_INT_VALIDATE\",\"id\":\"FILTER_VALIDATE_INT\",\"options\":{\"options\":{\"min_range\":1}}}'),
                 ('[Typesense] Enable Sync Log', 'TYPESENSE_ENABLE_SYNC_LOG', 'false', 'TODO', $configurationGroupId, now(), 5800, NULL, 'zen_cfg_select_option(array(\'true\', \'false\'),', NULL)
+        ";
+        $this->executeInstallerSql($sql);
+    }
+
+    /**
+     * Create the sync table.
+     *
+     * @return void
+     */
+    protected function createSyncTable(): void
+    {
+        $sql = "
+            CREATE TABLE IF NOT EXISTS " . DB_PREFIX . "typesense_sync_status (
+                id int NOT NULL DEFAULT '1',
+                status varchar(20) NOT NULL DEFAULT 'completed',
+                is_next_run_full tinyint(1) NOT NULL DEFAULT '0',
+                start_time datetime DEFAULT NULL,
+                end_time datetime DEFAULT NULL,
+                last_incremental_sync_start_time datetime DEFAULT NULL,
+                last_incremental_sync_end_time datetime DEFAULT NULL,
+                last_full_sync_start_time datetime DEFAULT NULL,
+                last_full_sync_end_time datetime DEFAULT NULL,
+                products_ids_to_delete text,
+                PRIMARY KEY (id)
+            ) ENGINE=InnoDB
+        ";
+        $this->executeInstallerSql($sql);
+
+        $sql = "
+            INSERT INTO " . DB_PREFIX . "typesense_sync_status
+                (id, status, is_next_run_full)
+            VALUES
+                (1, 'completed', 0)
+            ON DUPLICATE KEY UPDATE
+                id = 1
         ";
         $this->executeInstallerSql($sql);
     }
